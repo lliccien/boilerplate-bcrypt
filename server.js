c9825e4af3015a694ce25dff033b61c3677d1006
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-05-25 22:38:52 -0500

    execute bcrypt has and compare sync

--- server.js
+++ server.js
@@ -13,13 +13,20 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
 
 //END_ASYNC
 
 //START_SYNC
 
-
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 //END_SYNC
 
